9d9cea895fff57301b750b60fa7f2be4292acffd
Renan Augusto Romig <renanromig@hotmail.com>
2025-03-09 22:23:14 -0300

    add question 2

--- main.go
+++ main.go
@@ -4,7 +4,13 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("1. Create a program that displays the message 'Hello world' on the screen.\n")
+	fmt.Println("1. Create a program that displays the message", "Hello world", "on the screen.\n")
 
 	fmt.Println("Hello World\n")
+
+	fmt.Println("2. Create a program that requests a number and then displays the message", "The number entered was [number].\n")
+
+	var i int
+	fmt.Scan(&i)
+	fmt.Println("The number entered was", i)
 }
